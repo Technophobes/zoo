db34a1bbc81c0efb71012f4f9c43ea80bb817619
Leah Scott-Zechlin <scottzec@gmail.com>
2020-05-27 15:36:05 +0200

    data

---
+++ data.py
@@ -0,0 +1,21 @@
+## Define a list of dicts of our species.
+    species_list = [
+        {"common_name": "Domestic Cat", "scientific_name": "Felis catus", "genus": "Felis"},
+        {"common_name": "Black Footed cat", "scientific_name": "Felis nigripes", "genus": "Felis"},
+        {"common_name": "Jungle cat", "scientific_name": "Felis chaus", "genus": "Felis"},
+        {"common_name": "Domestic Dog", "scientific_name": "Canis familiaris", "genus": "Canis"},
+        {"common_name": "Elk", "scientific_name": "Cervus canadensis", "genus": "Cervus"},
+        {"common_name": "Giant amoeba", "scientific_name": "Chaos carolinense", "genus": "Chaos"},
+        {"common_name": "Common Bottlenose Dolphin", "scientific_name": "Tursiops truncatus", "genus": "Tursiops"},
+        {"common_name": "Sea otter", "scientific_name": "Enhydra lutris", "genus": "Enhydra"},
+        {"common_name": "Wolf", "scientific_name": "Canis lupus", "genus": "Canis"}
+    ]
+
+    ## Define a list of dicts of our specimens.
+    specimen_list = [
+        {"name": "bongo", "scientific_species": "Felis nigripes", "birth_date_time": "1262304000", "enclosure": "African Savannah", "continent": "Africa"},
+        {"name": "coco", "scientific_species": "Cervus canadensis", "birth_date_time": "1293840000", "enclosure": "Alaskan Tundra", "continent": "North America"},
+        {"name": "lola", "scientific_species": "Tursiops truncatus", "birth_date_time": "1325376000", "enclosure": "Under the Sea", "continent": "Asia"},
+        {"name": "shadow", "scientific_species": "Tursiops truncatus", "birth_date_time": "1356998400", "enclosure": "Under the Sea", "continent": "Asia"},
+        {"name": "stella", "scientific_species": "Enhydra lutris", "birth_date_time": "1420070400", "enclosure": "Alaskan Tundra", "continent": "North America"}
+    ]
