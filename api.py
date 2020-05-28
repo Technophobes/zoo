90f0d83eb4469ba9b01f24c3bce3099e91aed159
Aurélie FERRON <aurelieferron@Aurelies-MBP.fritz.box>
2020-05-28 12:25:38 +0200

    it should work

--- api.py
+++ api.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from flask import Flask
 from flask import jsonify
 from flask import request
+from model import dbconnect, Species, Specimen, Genus
+
 app = Flask(__name__)
 
 Base = declarative_base()
@@ -23,3 +25,9 @@ def cool():
         row_dict["birth_date_time"] = specimen.birth_date_time
         row_list.append(row_dict)
     return jsonify(row_list)
+
+# to get the error message on the url
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
--- data.py
+++ data.py
@@ -1,21 +1,52 @@
-## Define a list of dicts of our species.
-    species_list = [
-        {"common_name": "Domestic Cat", "scientific_name": "Felis catus", "genus": "Felis"},
-        {"common_name": "Black Footed cat", "scientific_name": "Felis nigripes", "genus": "Felis"},
-        {"common_name": "Jungle cat", "scientific_name": "Felis chaus", "genus": "Felis"},
-        {"common_name": "Domestic Dog", "scientific_name": "Canis familiaris", "genus": "Canis"},
-        {"common_name": "Elk", "scientific_name": "Cervus canadensis", "genus": "Cervus"},
-        {"common_name": "Giant amoeba", "scientific_name": "Chaos carolinense", "genus": "Chaos"},
-        {"common_name": "Common Bottlenose Dolphin", "scientific_name": "Tursiops truncatus", "genus": "Tursiops"},
-        {"common_name": "Sea otter", "scientific_name": "Enhydra lutris", "genus": "Enhydra"},
-        {"common_name": "Wolf", "scientific_name": "Canis lupus", "genus": "Canis"}
-    ]
+from sqlalchemy import Integer, Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relation
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask import Flask
+from flask import jsonify
+from flask import request
+from model import Species, Specimen, Genus, dbconnect
+app = Flask(__name__)
 
-    ## Define a list of dicts of our specimens.
-    specimen_list = [
-        {"name": "bongo", "scientific_species": "Felis nigripes", "birth_date_time": "1262304000", "enclosure": "African Savannah", "continent": "Africa"},
-        {"name": "coco", "scientific_species": "Cervus canadensis", "birth_date_time": "1293840000", "enclosure": "Alaskan Tundra", "continent": "North America"},
-        {"name": "lola", "scientific_species": "Tursiops truncatus", "birth_date_time": "1325376000", "enclosure": "Under the Sea", "continent": "Asia"},
-        {"name": "shadow", "scientific_species": "Tursiops truncatus", "birth_date_time": "1356998400", "enclosure": "Under the Sea", "continent": "Asia"},
-        {"name": "stella", "scientific_species": "Enhydra lutris", "birth_date_time": "1420070400", "enclosure": "Alaskan Tundra", "continent": "North America"}
-    ]
+from import_functions import addSpecimen, addSpecies
+from model import Species, Specimen, Genus, dbconnect
+
+Base = declarative_base()
+
+# A bunch of stuff to make the connection to the database work.
+engine = create_engine('sqlite:///zoo.db', echo=False)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
+# Define a list of dicts of our species.
+species_list = [
+    {"common_name": "Domestic Cat", "scientific_name": "Felis catus", "genus": {"scientific_name": "Felis"} },
+    {"common_name": "Black Footed cat", "scientific_name": "Felis nigripes", "genus": {"scientific_name": "Felis"}},
+    {"common_name": "Jungle cat", "scientific_name": "Felis chaus", "genus": {"scientific_name": "Felis"}},
+    {"common_name": "Domestic Dog", "scientific_name": "Canis familiaris", "genus": {"scientific_name": "Canis"}},
+    {"common_name": "Elk", "scientific_name": "Cervus canadensis", "genus": {"scientific_name": "Cervus"}},
+    {"common_name": "Giant amoeba", "scientific_name": "Chaos carolinense", "genus": {"scientific_name": "Chaos"}},
+    {"common_name": "Common Bottlenose Dolphin", "scientific_name": "Tursiops truncatus", "genus": {"scientific_name": "Tursiops"}},
+    {"common_name": "Sea otter", "scientific_name": "Enhydra lutris", "genus": {"scientific_name": "Enhydra"}},
+    {"common_name": "Wolf", "scientific_name": "Canis lupus", "genus": {"scientific_name": "Canis"}}
+] 
+
+for species in species_list:
+    addSpecies(session, species)
+
+
+# Define a list of dicts of our specimens.
+specimen_list = [
+    {"name": "bongo", "species": {"scientific_name": "Felis nigripes"}, "birth_date_time": "1262304000"},
+    {"name": "coco", "species": {"scientific_name": "Cervus canadensis"}, "birth_date_time": "1293840000"},
+    {"name": "lola", "species": {"scientific_name": "Tursiops truncatus"}, "birth_date_time": "1325376000"},
+    {"name": "shadow", "species": {"scientific_name": "Tursiops truncatus"}, "birth_date_time": "1356998400"},
+    {"name": "stella", "species": {"scientific_name": "Enhydra lutris"}, "birth_date_time": "1420070400"}
+]
+
+for specimen in specimen_list:
+    addSpecimen(session, specimen)
