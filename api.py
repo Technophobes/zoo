65e839f1178300c5585bb3e25401a8df69c0115d
Leah Scott-Zechlin <scottzec@gmail.com>
2020-05-29 13:29:26 +0200

    reconnect data to db

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from model import dbconnect, Species, Specimen, Genus
+from data import species_list
 
 app = Flask(__name__)
 
@@ -19,8 +20,24 @@ def cool():
         row_list.append(row_dict)
     return jsonify(row_list)
 
+@app.route('/species/<search_term>', methods=['GET'])
+def get_species(search_term):
+    session = dbconnect()
+    if request.method == 'GET':
+        return_list = []
+        print(search_term)
+        for row in session.query(Species).filter(Species.id == search_term).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
+        return jsonify(return_list)
+
+
+
+
 # to get the error message on the url
 if __name__ == '__main__':
 
     app.run(debug=True)
+
 
