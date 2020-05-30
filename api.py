b268b30d0b9299ee967f09422569f01049c69be1
Leah Scott-Zechlin <scottzec@gmail.com>
2020-05-30 20:46:38 +0200

    updated with species search

--- api.py
+++ api.py
@@ -23,34 +23,14 @@ def cool():
 @app.route('/species/<search_term>', methods=['GET'])
 def get_species(search_term):
     session = dbconnect()
-    if request.method == 'GET':
-        return_list = []
-        print(search_term)
-        for species in session.query(Species).filter(Species.id == search_term).all():
-            row_dict = row.__dict__
-            row_dict.pop("_sa_instance_state")
-            return_list.append(row_dict)
-        return jsonify(return_list)
+    return_list = []
+    print(search_term)
+    for row in session.query(Species).filter(Species.scientific_name == search_term).all():
+        row_dict = row.__dict__
+        row_dict.pop("_sa_instance_state")
+        return_list.append(row_dict)
+    return jsonify(return_list)
 
-
-@app.route('/species2/<search_term>', methods=['GET'])
-def search_species(search_term):
-    if request.method == 'GET':
-        return_list = []
-        session = dbconnect()
-        print(search_term)
-        for species in session.query(Species).filter(
-            Genus.id == search_term, 
-            Genus.id == Species.genus_id).all():
-            row = {} 
-            row["genus_id"] = genus.id
-            row["genus_scientific_name"] = genus.scientific_name
-            row["species_id"] = species.id
-            row["species_scientific_name"] = species.scientific_name
-            return_list.append(row)
-        return jsonify(return_list)
-    else:
-        return "Unsupported Method"
 
 # to get the error message on the url
 if __name__ == '__main__':
