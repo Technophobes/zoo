cdcad556ce461f7a573fb83e2d879c9c046ccf58
Andrew Holway <andrew.holway@gmail.com>
2020-06-04 16:34:44 +0200

    api working per docs

--- api.py
+++ api.py
@@ -3,111 +3,119 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from model import dbconnect, Species, Specimen, Genus
+from sqlalchemy import exc
 
 app = Flask(__name__)
 
-@app.route('/cool')
-def cool():
-    session = dbconnect()
-    specimens = session.query(Specimen).all()
-    # Serialisation starts here
-    row_list = []
-    for specimen in specimens:
-        row_dict = {}
-        row_dict["name"] = specimen.name
-        row_dict["birth_date_time"] = specimen.birth_date_time
-        row_list.append(row_dict)
-    return jsonify(row_list)
 
 
-
-@app.route('/add_genus', methods=['POST'])
+@app.route('/genus', methods=['POST'])
 def add_genus():
     session = dbconnect()
     request_dict = request.get_json()
-    try: 
-        genus_instance = session.query(Genus).filter(Genus.scientific_name == request_dict["scientific_name"]).one()
-        return "Genus already exists", 400
-    except:
+    try:
         genus_instance = Genus()
         genus_instance.scientific_name = request_dict["scientific_name"]
         session.add(genus_instance)
         session.commit()
-        return "ok"    
+        return "ok"
+    except exc.IntegrityError:
+        session.rollback()
+        return "already exists", 400
 
 
-@app.route('/add_species',  methods=['POST'])
+@app.route('/species',  methods=['POST'])
 def add_species():
     session = dbconnect()
     request_dict = request.get_json()
     try:
-        genus_instance = session.query(Genus).filter(Genus.scientific_name == request_dict["genus"]["scientific_name"]).one()
+        genus_instance = session.query(Genus).filter(Genus.id == request_dict["genus"]["id"]).one()
     except:
         return "Genus does not exist, please add it", 400
-    species = Species() 
-    species.scientific_name = request_dict["scientific_name"]
-    species.common_name = request_dict["common_name"]
-    species.genus_id = genus_instance.id
-    session.add(species)
-    session.commit()
-    return "ok"
+    
+    try:
+        species = Species() 
+        species.scientific_name = request_dict["scientific_name"]
+        species.common_name = request_dict["common_name"]
+        species.genus_id = genus_instance.id
+        session.add(species)
+        session.commit()
+        return "ok"
+    except exc.IntegrityError:
+        session.rollback()
+        return "already exists", 400
 
 
 
-@app.route('/add_specimen', methods=['POST'])
+
+@app.route('/specimen', methods=['POST'])
 def add_specimen(): 
     session = dbconnect()
     request_dict = request.get_json()
     try: 
-        species = session.query(Species).filter(Species.scientific_name == request_dict["species"]["scientific_name"]).one() #fix the database table so that there is only one 
+        species = session.query(Species).filter(Species.id == request_dict["species"]["id"]).one()
     except: 
         return "Species does not exist, please add it", 400
-    specimen = Specimen()
-    specimen.name = request_dict["name"]
-    specimen.birth_date_time = request_dict["birth_date_time"]
-    specimen.species_id = species.id    
-    session.add(specimen)
-    session.commit()
-    return "ok"
+    try:
+        specimen = Specimen()
+        specimen.name = request_dict["name"]
+        specimen.birth_date_time = request_dict["birth_date_time"]
+        specimen.species_id = species.id    
+        session.add(specimen)
+        session.commit()
+        return "ok"
+    except exc.IntegrityError:
+        session.rollback()
+        return "already exists", 400
 
 
-@app.route('/genus_and_species_and_specimen/<search_term>', methods=['GET'])
-def get_genus_and_species_and_specimen(search_term):
-    if request.method == 'GET':
-        return_list = []
-        session = dbconnect()
-
-        
-        for genus_instance, species_instance, specimen_instance in session.query(Genus, Species, Specimen).filter(
-            Genus.id == search_term, Genus.id == Species.genus_id, Species.id == Specimen.species_id).all():
-            
-            row = {}
-            row["genus_id"] = genus_instance.id
-            row["genus_scientific_name"] = genus_instance.scientific_name
-            row["species_id"] = species_instance.id
-            row["species_scientific_name"] = species_instance.scientific_name
-            row["specimen_id"] = specimen_instance.id
-            row["specimen_name"] = specimen_instance.name
-            return_list.append(row)
-        return jsonify(return_list)
+@app.route('/genus/<search_term>', methods=['GET'])
+def get_genus(search_term):
+    session = dbconnect()
+    return_list = []
+    if search_term == "all":
+        for row in session.query(Genus).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
     else:
-        return "Unsupported Method"
-
-
+        for row in session.query(Genus).filter(Genus.id == search_term).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
+    return jsonify(return_list)
 
 @app.route('/species/<search_term>', methods=['GET'])
 def get_species(search_term):
     session = dbconnect()
     return_list = []
-    print(search_term)
-    for row in session.query(Species).filter(Species.scientific_name == search_term).all():
-        row_dict = row.__dict__
-        row_dict.pop("_sa_instance_state")
-        return_list.append(row_dict)
+    if search_term == "all":
+        for row in session.query(Species).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
+    else:
+        for row in session.query(Species).filter(Species.id == search_term).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
     return jsonify(return_list)
 
-
-
+@app.route('/specimen/<search_term>', methods=['GET'])
+def get_specimen(search_term):
+    session = dbconnect()
+    return_list = []
+    if search_term == "all":
+        for row in session.query(Specimen).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
+    else:
+        for row in session.query(Specimen).filter(Specimen.id == search_term).all():
+            row_dict = row.__dict__
+            row_dict.pop("_sa_instance_state")
+            return_list.append(row_dict)
+    return jsonify(return_list)
 
 
 
--- model.py
+++ model.py
@@ -47,7 +47,7 @@ class Specimen(Base):
 
 # A bunch of stuff to make the connection to the database work.
 def dbconnect():
-    engine = create_engine('sqlite:///zoo.db', echo=True)
+    engine = create_engine('sqlite:///zoo.db', echo=False)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     return Session()
