0358184400c16cc7663b13f573a501d159612a0c
Leah Scott-Zechlin <scottzec@gmail.com>
2020-06-10 18:38:11 +0200

    test written as 3 functions

--- tests/test1.py
+++ tests/test1.py
@@ -1,32 +1,54 @@
-# Test that there is indeed an error when a duplicate is added (identical genus, species and specimen name)
-# Duplicate should not be added to the database
-# Expected output: In Terminal "already exists" (referring to the 2nd dictionary in test_list) and 1 entry for "irbis" added to DB
+# Tests if there is an error when a duplicate genus, species or specimen is added to the database
+# Exit(0) means test is a success
 
 import requests
 import json
 
-test_list = [
-    {"name": "irbis", "dob": "1362304000", "species_common": "snow leopard", "species_scientific": "panthera uncia", "genus_scientific": "panthera"}, 
-    {"name": "irbis", "dob": "1362304000", "species_common": "snow leopard", "species_scientific": "panthera uncia", "genus_scientific": "panthera"} 
-]
 
-for input_dict in test_list:    
-    pload_genus = {"scientific_name": input_dict["genus_scientific"]}
-    genus_request = requests.post("http://127.0.0.1:5000/genus" , json=pload_genus)
-    genus_id = genus_request.text
-    print(genus_id)
-    
-    if genus_request.status_code == 200: 
-        pload_species = {"scientific_name": input_dict["species_scientific"], "common_name" : input_dict["species_common"], "genus" : { "id" : genus_id}}
-        species_request = requests.post("http://127.0.0.1:5000/species" , json=pload_species)
-        species_id = species_request.text   
-        print(species_id)
-    else :
-        exit(1)
+def genus_request(genus_dict):
+    genus_request1 = requests.post("http://127.0.0.1:5000/genus" , json=genus_dict)
+    genus_request2 = requests.post("http://127.0.0.1:5000/genus" , json=genus_dict)
 
-    if species_request.status_code == 200:
-        pload_specimen = {"name": input_dict["name"], "birth_date_time" : input_dict["dob"], "species" : {"id" : species_id}}
-        specimen_requests = requests.post("http://127.0.0.1:5000/specimen" , json=pload_specimen)
-    
+    if genus_request1.status_code == 200 and genus_request2.status_code == 400:
+        # returns genus ID
+        return genus_request1.text
     else:
+        print("Something went wrong adding genus")
         exit(1)
+
+
+def species_request(species_dict):
+    species_request1 = requests.post("http://127.0.0.1:5000/species" , json=species_dict)
+    species_request2 = requests.post("http://127.0.0.1:5000/species" , json=species_dict)
+
+    if species_request1.status_code == 200 and species_request2.status_code == 400:
+        # returns species ID
+        return species_request1.text
+    else:
+        print("Something went wrong adding species")
+        exit(1)
+
+
+def specimen_request(specimen_dict):
+    specimen_request1 = requests.post("http://127.0.0.1:5000/specimen" , json=specimen_dict)
+    specimen_request2 = requests.post("http://127.0.0.1:5000/specimen" , json=specimen_dict)
+
+    if specimen_request1.status_code == 200 and specimen_request2.status_code == 400:
+        # returns specimen ID
+        return specimen_request1.text
+    else:
+        print("Something went wrong with adding specimen")
+        exit(1)
+
+
+genus_dict = {"scientific_name":"panthera"}
+
+genus_id = genus_request(genus_dict)
+
+species_dict = {"common_name": "snow leopard", "scientific_name": "panthera uncia", "genus": {"id": genus_id} }
+
+species_id = species_request(species_dict)
+
+specimen_dict = {"name": "irbis", "species": {"id": species_id}, "birth_date_time": "1262304000"}
+
+specimen_id = specimen_request(specimen_dict)
